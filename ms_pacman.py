aa8d6fa0ebfc137f7725ffa4478ce6f356c65435
Anass Al <mr@anassinator.com>
2016-11-10 00:45:07 -0500

    Add agent directions

--- ms_pacman.py
+++ ms_pacman.py
@@ -36,6 +36,8 @@ class MsPacManGame(object):
         self._ale.loadROM("MS_PACMAN.BIN")
 
         self._reward = 0
+        self._ms_pacman_direction = self._ms_pacman_position = (0, 0)
+        self._ghost_directions = self._ghost_positions = [(0, 0)] * 4
 
         self.__ram = self._ale.getRAM()
         self._update_state()
@@ -54,6 +56,16 @@ class MsPacManGame(object):
     def ghost_positions(self):
         """Ghost positions as a list of (x, y) tuples."""
         return self._ghost_positions
+
+    @property
+    def ms_pacman_direction(self):
+        """Ms. PacMan's direction as an (x, y) tuple."""
+        return self._ms_pacman_direction
+
+    @property
+    def ghost_directions(self):
+        """Ghost directions as a list of (x, y) tuples."""
+        return self._ghost_directions
 
     def available_actions(self):
         """Returns a list of available actions to consider."""
@@ -89,13 +101,44 @@ class MsPacManGame(object):
         self._reward = 0
         return self._ale.reset_game()
 
+    def _get_direction(self, prev, curr, prev_direction):
+        """Computes the relative direction from one position to another.
+
+        Args:
+            prev: Previous (x, y) position.
+            curr: Current (x, y) position.
+            prev_direction: Previous direction.
+
+        Returns:
+            Relative direction.
+        """
+        new_direction = (curr[0] - prev[0], curr[1] - prev[1])
+        return new_direction if new_direction != (0, 0) else prev_direction
+
     def _update_state(self):
         """Updates the internal state of the game."""
         self._ale.getRAM(self.__ram)
-        self._ms_pacman_position = (self.__ram[10], self.__ram[16])
-        self._ghost_positions = [
-            (self.__ram[6], self.__ram[12]),
-            (self.__ram[7], self.__ram[13]),
-            (self.__ram[8], self.__ram[14]),
-            (self.__ram[9], self.__ram[15])
+
+        # Get new states.
+        new_ms_pacman_position = (int(self.__ram[10]), int(self.__ram[16]))
+        new_ghost_positions = [
+            (int(self.__ram[6]), int(self.__ram[12])),
+            (int(self.__ram[7]), int(self.__ram[13])),
+            (int(self.__ram[8]), int(self.__ram[14])),
+            (int(self.__ram[9]), int(self.__ram[15]))
         ]
+
+        # Update directions.
+        self._ms_pacman_direction = self._get_direction(
+            self._ms_pacman_position, new_ms_pacman_position,
+            self._ms_pacman_direction)
+        self._ghost_directions = [
+            self._get_direction(self._ghost_positions[i],
+                                new_ghost_positions[i],
+                                self._ghost_directions[i])
+            for i in range(len(new_ghost_positions))
+        ]
+
+        # Update positions.
+        self._ms_pacman_position = new_ms_pacman_position
+        self._ghost_positions = new_ghost_positions
